ed6034f7e97c15df7ee04eaf01fbf957e54f7fb1
harshbsv <harshsolo.dev@gmail.com>
2021-04-29 22:19:58 +0530

    - Completed app

--- Dicee/ContentView.swift
+++ Dicee/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var leftDiceNumber = 1
+    @State var rightDicenumber = 1
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Image("diceeLogo")
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+                    .frame(width: 220, height: 200, alignment: .center)
+                Spacer()
+                HStack{
+                    Dice(diceNumber: leftDiceNumber)
+                    Dice(diceNumber: rightDicenumber)
+                }.padding()
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDicenumber = Int.random(in: 1...6)
+                }, label: {
+                    Text("Roll")
+                        .font(.system(size: 30))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding()
+                }).background(Color.red)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice("iPhone 12 Pro Max")
+    }
+}
+
+struct Dice: View {
+    let diceNumber: Int
+    var body: some View {
+        Image("dice\(diceNumber)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .frame(width: 150, height: 150, alignment: .center)
+            .padding()
     }
 }
