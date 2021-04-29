7a10f04db456cc09c8bb315ca85dedddbd7ac274
harshbsv <harshsolo.dev@gmail.com>
2021-04-29 21:08:40 +0530

    Initial Commit

---
+++ Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Harshvardhan Basava on 29/04/21.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
